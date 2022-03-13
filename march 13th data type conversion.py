60919daf3b5f8b8a829a5019b22280ce6af76375
preethianu <101032856+preethianu@users.noreply.github.com>
2022-03-13 11:31:08 +0530

    Add files via upload

---
+++ march 13th data type conversion.py
@@ -0,0 +1,20 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+a=10.2
+b=float(a)
+print(b)
+10.2
+print(type(b))
+<class 'float'>
+a="Hello world"
+b=str(a)
+print(b)
+Hello world
+print(type(b))
+<class 'str'>
+a=1
+b=bool(a)
+print(b)
+True
+print(type(b))
+<class 'bool'>
