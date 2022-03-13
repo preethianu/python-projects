d4463c6e4f93791a8ad47d4528b949048d27c542
preethianu <101032856+preethianu@users.noreply.github.com>
2022-03-13 11:30:41 +0530

    Add files via upload

---
+++ march 13 task session.py
@@ -0,0 +1,47 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/vsgop/OneDrive/Desktop/python task session/perimeter of the circle.py
+Traceback (most recent call last):
+  File "C:/Users/vsgop/OneDrive/Desktop/python task session/perimeter of the circle.py", line 4, in <module>
+    print("Perimeter of the circle",circle)
+NameError: name 'circle' is not defined
+
+= RESTART: C:/Users/vsgop/OneDrive/Desktop/python task session/perimeter of the circle.py
+Perimeter of the circle 64.056
+r=10.2
+pi=3.14
+c=2*pi*r
+print("perimeter of the circle",c)
+perimeter of the circle 64.056
+a=float(input("enter the value:")
+        enter r value =23
+        
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+Enter R value:23
+        
+SyntaxError: invalid syntax
+enter r value:23
+        
+SyntaxError: invalid syntax
+enter area of the value:23
+        
+SyntaxError: invalid syntax
+enter a of the value:23
+        
+SyntaxError: invalid syntax
+a=float(input("Enter the R value:"))
+        
+Enter the R value:23
+area=3.14*a*a
+        
+print("Area of the circle=",area)
+        
+Area of the circle= 1661.06
+h=12
+        
+cone=0.33*pi*r*r*h
+        
+print("area of the cone=",cone)
+        
+area of the cone= 1293.6749759999998
