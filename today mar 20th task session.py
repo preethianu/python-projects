900d7b47a68edf01e40e0e0f7654dee474c3b4c1
preethianu <101032856+preethianu@users.noreply.github.com>
2022-03-20 13:28:56 +0530

    Add files via upload

---
+++ today mar 20th task session.py
@@ -0,0 +1,42 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+a="hello world"
+print(a)
+hello world
+a="computer science"
+print(a.swapcase())
+COMPUTER SCIENCE
+a="cOMPuter SCIEnce"
+print(a.swapcase())
+CompUTER scieNCE
+print(a.title())
+Computer Science
+print(a.capitalize())
+Computer science
+a="python"
+l=list(a)
+l[3]="k"
+b="".join(l)
+print(b)
+pytkon
+x=9
+y=7
+print(x+y)
+16
+print(x*y)
+63
+print(x-y)
+2
+a=7
+b=5
+print(a//b)
+1
+print(a/b)
+1.4
+
+a=" python "
+print(a.strip())
+
+
+
+
