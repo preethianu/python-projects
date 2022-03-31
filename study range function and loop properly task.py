01e6505ad9a1eab6d3c818f1b4ce637a25c5ace3
preethianu <101032856+preethianu@users.noreply.github.com>
2022-03-31 11:10:44 +0530

    Add files via upload

---
+++ study range function and loop properly task.py
@@ -0,0 +1,29 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+result=range(5)
+print(list(result))
+[0, 1, 2, 3, 4]
+result=range(1,5)
+print(list(result))
+[1, 2, 3, 4]
+result=range(5,20)
+print(list(result))
+[5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+x=range(-5,5)
+print(list(x))
+[-5, -4, -3, -2, -1, 0, 1, 2, 3, 4]
+x=range(10,100,10)
+print(list(x))
+[10, 20, 30, 40, 50, 60, 70, 80, 90]
+x=range(10,0,-1)
+print(list(x))
+[10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+x=range(-5,5,3)
+print(list(x))
+[-5, -2, 1, 4]
+x=range(10,100,5)
+print(list(x))
+[10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95]
+x=range(10,0,-2)
+print(list(x))
+[10, 8, 6, 4, 2]
