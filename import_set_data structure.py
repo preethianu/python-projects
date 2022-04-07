799de4a451cf5b13d2b4d1034c6e7dba17672204
preethianu <101032856+preethianu@users.noreply.github.com>
2022-04-07 12:44:37 +0530

    Add files via upload

---
+++ import_set_data structure.py
@@ -0,0 +1,19 @@
+#import set
+#create a set
+thisset={1,2,3,4}
+print(thisset)
+
+#update a set
+set1={"apple","banana","mango"}
+set2={1,2,3}
+set1.update(set2)
+print(set1)
+
+#intersetion of set
+set3={"apple","banana","cherry"}
+set4={"pineapple","grapes","apple"}
+set5=set3.intersection(set4)
+print(set5)
+
+
+      
